ad180f6d26f323fdce685d8a6ab25a1d03fb36d8
q36644991-max <q36644991@gmail.com>
2026-05-15 10:25:20 +0530

    Delete admin directory

--- admin/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=$PYTHONPATH:.
-
-# Start Auth service in the background
-gunicorn -b 0.0.0.0:5001 admin.backend.auth_service.app:app &
-
-# Start Management Service in the foreground on the Render port
-gunicorn -b 0.0.0.0:$PORT admin.backend.management_service:app
